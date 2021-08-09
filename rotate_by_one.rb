a06f6ded1e288887f16bead300f4588fe4192012
Mederic Gbagba <medericjoel@gmail.com>
2021-08-09 21:49:38 +0000

    Resolve rotate_by_one

--- rotate_by_one.rb
+++ rotate_by_one.rb
@@ -1,6 +1,24 @@
-def rotate_by_one(arr)
-    arr_len = arr.length - 1
+def rotate_by_one(arr) 
+    temp = arr[-1]
+    i =  arr.length - 1
+    while i > 0
+        arr[i] = arr[i - 1]
+        i -= 1
+    end
+    arr[0] = temp
+    p arr
 end
+
+# def rotate_by_one(arr) 
+#     temp = arr[-1]
+#     i =  arr.length - 1
+#     for n in arr
+#         arr[i] = arr[i - 1]
+#         i -= 1
+#     end
+#     arr[0] = temp
+#     p arr
+# end
 
 rotate_by_one([1, 2, 3, 4, 5]) # [5, 1, 2, 3, 4]
 rotate_by_one([6, 5, 8, 9, 7]) # [7, 6, 5, 8, 9]
