50ef5711516ced2750d315d200b93142a5a3c9b7
Mederic Gbagba <medericjoel@gmail.com>
2021-08-09 21:58:23 +0000

    Resolve remove_duplicate

--- remove_duplicate.rb
+++ remove_duplicate.rb
@@ -1,11 +1,11 @@
 def remove_dups(arr)
-    new_arr = []
-    for i in arr
-        new_arr.push(i)
+    new_hash = Hash.new
+    arr.each do |arr_el|
+        new_hash[arr_el] = arr_el
     end
-    p new_arr
+    p new_hash.keys
 end
 
 remove_dups([1, 0, 1, 0]) # [1, 0]
-# remove_dups(["The", "big", "cat"]) # ["The", "big", "cat"]
-# remove_dups(["John", "Taylor", "John"]) # ["John", "Taylor"]
+remove_dups(["The", "big", "cat"]) # ["The", "big", "cat"]
+remove_dups(["John", "Taylor", "John"]) # ["John", "Taylor"]
